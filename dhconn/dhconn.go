b7edf4e89b95668424a274c5485ad0014b0ccd6a
Beck <iz596192@ucf.edu>
2024-08-19 14:38:58 -0400

    update

--- dhconn/dhconn.go
+++ dhconn/dhconn.go
@@ -4,16 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"godehashed/parsedh"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
 
 const DURI string = "https://api.dehashed.com/search?query="
+
+func makeRequest(url, username, password string) ([]byte, error) {
+	client := http.Client{Timeout: 30 * time.Second}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating request failed: %w", err)
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+	req.Header.Add("Accept", "application/json")
+	req.SetBasicAuth(username, password)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("executing request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response failed: %w", err)
+	}
+	return body, nil
+}
 
 func DHConn(apikey, email, name, searchterm, uname, outfile, elist string, phone int) {
 	username := strings.Split(strings.Trim(apikey, "\n"), ":")[0]
@@ -23,73 +45,37 @@ func DHConn(apikey, email, name, searchterm, uname, outfile, elist string, phone
 	switch searchterm {
 	case "email":
 		fmt.Println("[*] Searching for emails.")
-		client := http.Client{}
-		req, err := http.NewRequest("GET", DURI+"email:"+email+`&size=10000`+"&"+"page="+strconv.Itoa(pages), nil)
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-		req.Header.Add("Accept", "application/json")
-		req.SetBasicAuth(username, password)
-
+		url := fmt.Sprintf("%s%s:%s&size=10000&page=%d", DURI, searchterm, email, pages)
+		body, err := makeRequest(url, username, password)
 		if err != nil {
-			log.Fatal("Dehash connection error: ", err)
-		}
-		resp, err2 := client.Do(req)
-
-		if err2 != nil {
-			log.Fatal("Error in fetching response ", err2)
-		}
-
-		defer resp.Body.Close()
-
-		body, err3 := ioutil.ReadAll(resp.Body)
-		if err3 != nil {
-			log.Fatal("Response Error: ", err3)
+			log.Fatalf("Dehashed connection error: %v", err)
 		}
 
 		total, balance := parsedh.ParseDH(body, outfile)
-
 		if balance == 0 {
 			fmt.Println("[!] You're out of credits, please reload then run again.")
 			os.Exit(0)
 		}
 
-		fmt.Println("[*] Total number of results: " + strconv.Itoa(total))
+		fmt.Println("[*] Total number of results:", total)
 
 		if total >= 10000 {
-			fmt.Println("[!] ATTENTION!! Total number of results greater than 100, do you want to continue? ENTER: 'Y' or 'N' to exit.")
+			fmt.Println("[!] ATTENTION!! Total number of results greater than 10,000. Do you want to continue? ENTER: 'Y' or 'N' to exit.")
 			var response string
-			_, err := fmt.Scanln(&response)
-
-			if err != nil {
-				log.Fatal(err)
+			if _, err := fmt.Scanln(&response); err != nil {
+				log.Fatalf("Input error: %v", err)
 			}
 
 			if response == "Y" || response == "y" {
-				for pages := 2; pages <= 3; pages++ {
-					fmt.Println("[!] Current Page is:" + strconv.Itoa(pages))
+				for pages = 2; pages <= 3; pages++ {
+					fmt.Println("[!] Current Page is:", pages)
 					time.Sleep(5 * time.Second)
 					fmt.Println("[*] We are searching for emails.")
-					client := http.Client{}
-					req, err := http.NewRequest("GET", DURI+"email:"+email+`&size=10000`+"&"+"page="+strconv.Itoa(pages), nil)
-					req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-					req.Header.Add("Accept", "application/json")
-					req.SetBasicAuth(username, password)
-
+					url := fmt.Sprintf("%s%s:%s&size=10000&page=%d", DURI, searchterm, email, pages)
+					body, err := makeRequest(url, username, password)
 					if err != nil {
-						log.Fatal("Dehash connection error: ", err)
+						log.Fatalf("Dehashed connection error: %v", err)
 					}
-					resp, err2 := client.Do(req)
-
-					if err2 != nil {
-						log.Fatal("Error in fetching response ", err2)
-					}
-
-					defer resp.Body.Close()
-
-					body, err3 := ioutil.ReadAll(resp.Body)
-					if err3 != nil {
-						log.Fatal("Response Error: ", err3)
-					}
-
 					parsedh.ParseDH(body, outfile)
 				}
 			} else {
@@ -98,169 +84,96 @@ func DHConn(apikey, email, name, searchterm, uname, outfile, elist string, phone
 		}
 
 	case "name":
-		var searchname string
-
-		searchname = strings.ReplaceAll(name, " ", "+")
-
+		searchname := strings.ReplaceAll(name, " ", "+")
 		fmt.Println("[*] We are searching for names.")
-		client := http.Client{}
-		req, err := http.NewRequest("GET", DURI+"name:"+searchname, nil)
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-		req.Header.Add("Accept", "application/json")
-		req.SetBasicAuth(username, password)
-
+		url := fmt.Sprintf("%sname:%s", DURI, searchname)
+		body, err := makeRequest(url, username, password)
 		if err != nil {
-			log.Fatal("Dehash connection error: ", err)
-		}
-		resp, err2 := client.Do(req)
-
-		if err2 != nil {
-			log.Fatal("Error in fetching response ", err2)
+			log.Fatalf("Dehashed connection error: %v", err)
 		}
 
-		defer resp.Body.Close()
-
-		body, err3 := ioutil.ReadAll(resp.Body)
-		if err3 != nil {
-			log.Fatal("Response Error: ", err3)
-		}
 		total, balance := parsedh.ParseDH(body, outfile)
-
 		if balance == 0 {
 			fmt.Println("[!] You're out of credits, please reload then run again.")
 			os.Exit(0)
 		}
 
-		fmt.Println("[*] Total number of results: " + strconv.Itoa(total))
+		fmt.Println("[*] Total number of results:", total)
 
 	case "phone":
 		fmt.Println("[*] We are searching for phone numbers.")
-		client := http.Client{}
-		req, err := http.NewRequest("GET", DURI+"phone:"+strconv.Itoa(phone), nil)
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-		req.Header.Add("Accept", "application/json")
-		req.SetBasicAuth(username, password)
-
+		url := fmt.Sprintf("%sphone:%d", DURI, phone)
+		body, err := makeRequest(url, username, password)
 		if err != nil {
-			log.Fatal("Dehash connection error: ", err)
-		}
-		resp, err2 := client.Do(req)
-
-		if err2 != nil {
-			log.Fatal("Error in fetching response ", err2)
+			log.Fatalf("Dehashed connection error: %v", err)
 		}
 
-		defer resp.Body.Close()
-
-		body, err3 := ioutil.ReadAll(resp.Body)
-		if err3 != nil {
-			log.Fatal("Response Error: ", err3)
-		}
 		total, balance := parsedh.ParseDH(body, outfile)
-
 		if balance == 0 {
 			fmt.Println("[!] You're out of credits, please reload then run again.")
 			os.Exit(0)
 		}
 
-		fmt.Println("[*] Total number of results: " + strconv.Itoa(total))
+		fmt.Println("[*] Total number of results:", total)
 
 	case "username":
-		fmt.Println("[*] We are searching for Usernames.")
-		client := http.Client{}
-		req, err := http.NewRequest("GET", DURI+"username:"+uname, nil)
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-		req.Header.Add("Accept", "application/json")
-		req.SetBasicAuth(username, password)
-
+		fmt.Println("[*] We are searching for usernames.")
+		url := fmt.Sprintf("%susername:%s", DURI, uname)
+		body, err := makeRequest(url, username, password)
 		if err != nil {
-			log.Fatal("Dehash connection error: ", err)
-		}
-		resp, err2 := client.Do(req)
-
-		if err2 != nil {
-			log.Fatal("Error in fetching response ", err2)
-		}
-
-		defer resp.Body.Close()
-
-		body, err3 := ioutil.ReadAll(resp.Body)
-		if err3 != nil {
-			log.Fatal("Response Error: ", err3)
+			log.Fatalf("Dehashed connection error: %v", err)
 		}
 
 		total, balance := parsedh.ParseDH(body, outfile)
-
 		if balance == 0 {
 			fmt.Println("[!] You're out of credits, please reload then run again.")
 			os.Exit(0)
 		}
 
-		fmt.Println("[*] Total number of results: " + strconv.Itoa(total))
+		fmt.Println("[*] Total number of results:", total)
 
 	case "list":
-		fmt.Print("[*] Going into List mode, Will add a 2 second time delay to prevent blacklist.")
-		fmt.Print("[*] ATTENTION: This can take a long time depeding on the size of the list and will use ALOT OF CREDITS!.\n")
+		fmt.Println("[*] Going into List mode, will add a 2-second time delay to prevent blacklist.")
+		fmt.Println("[*] ATTENTION: This can take a long time depending on the size of the list and will use A LOT OF CREDITS!")
 		time.Sleep(2 * time.Second)
-		//opens list of files
+
 		file, err := os.Open(elist)
 		if err != nil {
-			fmt.Println("Cannot read file", err)
-			os.Exit(0)
+			log.Fatalf("Cannot read file: %v", err)
 		}
-
 		defer file.Close()
 
 		scanner := bufio.NewScanner(file)
-		scanner.Split(bufio.ScanLines)
-
 		var dhlist []string
 
 		for scanner.Scan() {
 			dhlist = append(dhlist, scanner.Text())
 		}
-
-		file.Close()
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error reading file: %v", err)
+		}
 
 		for _, line := range dhlist {
 			fmt.Println("[*] We are searching for emails.")
-			client := http.Client{}
-			req, err := http.NewRequest("GET", DURI+line, nil)
-			req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-			req.Header.Add("Accept", "application/json")
-			req.SetBasicAuth(username, password)
-
+			url := fmt.Sprintf("%s%s", DURI, line)
+			body, err := makeRequest(url, username, password)
 			if err != nil {
-				log.Fatal("Dehash connection error: ", err)
-			}
-			resp, err2 := client.Do(req)
-
-			if err2 != nil {
-				log.Fatal("Error in fetching response ", err2)
+				log.Fatalf("Dehashed connection error: %v", err)
 			}
 
-			defer resp.Body.Close()
-
-			body, err3 := ioutil.ReadAll(resp.Body)
-			if err3 != nil {
-				log.Fatal("Response Error: ", err3)
-			}
 			total, balance := parsedh.ParseDH(body, outfile)
-
 			if balance == 0 {
 				fmt.Println("[!] You're out of credits, please reload then run again.")
 				os.Exit(0)
 			}
 
-			fmt.Println("[*] Total number of results: " + strconv.Itoa(total))
-
-			fmt.Println("[*] Delaying.. Please wait.")
+			fmt.Println("[*] Total number of results:", total)
+			fmt.Println("[*] Delaying... Please wait.")
 			time.Sleep(2 * time.Second)
 		}
 
 	default:
-		fmt.Println("Please enter in a valid search term, 'email' or 'name'.")
+		fmt.Println("Please enter a valid search term: 'email', 'name', 'phone', 'username', or 'list'.")
 		os.Exit(0)
 	}
-
 }
